0a88c514d71dfe9239317791f17e84143dc39039
Grant Knief <gknief@mac.com>
2022-09-05 13:56:10 -0400

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 
 function App() {
+  const [coins, setCoins] = useState([])
 
-
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    })
+    .catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
